fa24c856c097b31fb0dfe9fb7cbaed5c9102e4d0
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-03-12 22:46:38 +0530

    updated

---
+++ motiontel/first.cpp
@@ -0,0 +1,4 @@
+/*Copyrights (C) Neksha DeSilva and MotionTel Networks. All Rights Reserved */
+/*Warning: This code is licensed under GNU GPL Verison 3.0 LICENSE*/
+/*https://status.motiontel.net*/
+
