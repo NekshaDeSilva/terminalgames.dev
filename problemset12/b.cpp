4745587d3901a4d0cb19d8cceb637d0b0e8c030f
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-03-14 22:27:56 +0530

    updated

--- problemset12/b.cpp
+++ problemset12/b.cpp
@@ -6,11 +6,45 @@
 #include <cmath>
 
 using namespace std;
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int averagedisplayer(const vector<long long int>& passedvactor){
+    int totval =0;
 
+    int temp89248;
+    if(passedvactor.size() > 3){
+        for(int i = 0; i < passedvactor.size(); i++){
+    totval += passedvactor[i];
+    temp89248 = i;
+        
+    }
+    int result =totval/passedvactor.size();
+
+    cout << "\n" << result << "\n";
+
+    }else{
+        
+    }
     
+    return 0;
+
+
+}
+int main(){
+
+    vector<long long int> listofnums;
+    listofnums.reserve(400);
+    int counter22 =0;
+   int tempval = 0;
+
+    while(cin >> tempval)
+    {
+
+        
+      
+        listofnums.push_back(tempval);
+        averagedisplayer(listofnums);
+        tempval=0;
+    }
+
 
 
 }
---
+++ problemset12/c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <array>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main(){
+    ios_main::sync_with_stdio(0);
+    cin.tie(NULL);
+
+}
