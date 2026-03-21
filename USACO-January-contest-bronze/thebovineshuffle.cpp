16cf2f460724f08b013d1e59383b1bfe40d44f8e
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-03-21 19:57:04 +0530

    udpated

---
+++ USACO-January-contest-bronze/thebovineshuffle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <random>
+#include <cmath>
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("shuffle.in", "r", stdin);
+    freopen("shuffle.out", "w", stdout);
+    //do
+
+    
+}
