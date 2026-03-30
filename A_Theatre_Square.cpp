766be2d5c4251ae2821b8c38d87e93d52f25933d
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-03-30 19:41:10 +0530

    updated

---
+++ A_Theatre_Square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <random>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //go
+    int n, m, a ;
+    cin >> n >> m >> a;
+    
+}
