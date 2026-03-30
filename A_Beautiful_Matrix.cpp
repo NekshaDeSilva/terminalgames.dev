e15574b3ac14616c859bbc0294529846d9f78530
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-03-30 17:58:26 +0530

    updated

---
+++ A_Beautiful_Matrix.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <vector>
+#include <random>
+using namespace std;
+int main()[
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //go
+
+    vector<vector<int>> matrix2;
+    matrix2.reserve(1000);
+    
+]
--- A_Games.cpp
+++ A_Games.cpp
@@ -11,5 +11,30 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //go
+    vector<pair<int, int>> teams;
+    teams.reserve(1000);
+    int numofteams=0;
+    cin >> numofteams;
+    int temp24 ,temp25;
+
+    int numofinc= 0;
+    for(int i= 0; i < numofteams; i++){
+        cin >> temp24 >> temp25;
+        teams.emplace_back(temp24, temp25);
+
+    }
+    for(int i = 0 ; i < numofteams; i++){
+        for(int x = i+1; x < numofteams;x++ ){
+            if(teams[i].first == teams[x].second){
+                    numofinc++;
+                   
+                }
+            if(teams[i].second == teams[x].first){
+                    numofinc++;
+                }
+        }
+    }   
+
+    cout << numofinc;
 
 }
