7c7013e857aa2d4f8bd025b1331f83682ef07e59
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-04 23:18:54 +0530

    updated

---
+++ C_Fibonacci.cpp
@@ -0,0 +1,28 @@
+//this is just  a test
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <array>
+#include <random>
+
+using namespace std;
+
+
+int recurse(int userreq, int sec, int thr){
+    if(userreq == tjhr){
+        cout << 0;
+
+    }
+    return start + recurse(start);
+}
+int main(){
+    int start = 1;
+    int userreq;
+    cin >> userreq;
+    int result =  recurse(userreq, 0, 1);
+    cout << result;
+
+}   
