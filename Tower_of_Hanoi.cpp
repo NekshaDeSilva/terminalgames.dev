be871760b9a5d96b0238579f64afc2250e30b09a
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-04 10:33:20 +0530

    updated

--- Tower_of_Hanoi.cpp
+++ Tower_of_Hanoi.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cmath>
 #include <vector>
 #include <array>
-#include <cmath>
-#include <random>
 #include <algorithm>
-using namespace std;
-using ll = long long;
+#include <random>
 
-int recurse(){
+using namespace std;
+void recurse(int numofdisks, int start, int end, int middle){
+    if(numofdisks ==1){
+        cout << start << " " << end << "\n";
+        return;
+
+    }
+    recurse(numofdisks-1, start, middle, end);
+    cout << start << " "  << end << "\n";
+    recurse(numofdisks-1, middle, end, start);
+
 
 }
 int main(){
-    
+
+    int numofdisks = 0;
+    cin >> numofdisks;
+    int start = 1; int middle = 2;
+    int end = 3;
+    cout << (1 << numofdisks)-1 << "\n";
+    recurse(numofdisks, start, end, middle);
+
+
 }
