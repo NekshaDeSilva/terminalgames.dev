fea47aef4747528d3930161ae181a228017042a9
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-04 16:01:58 +0530

    updated

---
+++ Sacred_Caravan.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+void recurse(){
+
+    
+
+}
+int main(){
+
+int numofdisks = 0;
+cin >> numofdisks;
+int diska = 0;
+int diskb = 0;
+int diskc = 0;
+cout << (1 << numofdisks) -1 << "\n";
+
+
+
+}
