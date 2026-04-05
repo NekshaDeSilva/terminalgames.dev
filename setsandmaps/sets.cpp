32f8a5d36223a5778130bee0e56a9d139c3d4db5
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-05 23:48:17 +0530

    updated

--- setsandmaps/sets.cpp
+++ setsandmaps/sets.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <array>
-
+#include <vector>
+#include <random>
+#define all(x) (x).begin(), (x).end()
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    
+    random_device somerandom;
+    mt19937 gen(somerandom());
+    uniform_int_distribution<> distr(1,10);
+
+    vector<int> somearray;
+    for(int i =0 ; i < 10;  i ++){
+        int randomvalks = distr(gen);
+        somearray.push_back(randomvalks);
+    }
+    all(somearray);
+    for(int i =0;  i < 10; i++){
+        cout << somearray[i] << "\n";
+    }
 }
