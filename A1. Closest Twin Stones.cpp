83ebc979479abe44bf9a827ce687f07512dc0afd
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-06 20:26:50 +0530

    upated

--- A1. Closest Twin Stones.cpp
+++ A1. Closest Twin Stones.cpp
@@ -5,11 +5,33 @@
 #include <array>
 #include <cmath>
 #include <random>
-
 using ll = long long;
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int numofstones;
+    cin >> numofstones;
+
+    vector<int> stonew;
+    stonew.reserve(1000);
+    int tempval ;
+    for(int  i =0; i < numofstones; i++){
+       cin >> tempval; 
+       stonew.push_back(tempval);
+    }
+    sort(stonew.begin() ,stonew.end());
+    int min =0;
+    vector<int> minvals;
+    minvals.reserve(numofstones);
+    for(int i =0; i < numofstones; i ++){
+        for(int x = i+1; x< numofstones;  x++){
+            min = abs(stonew[i] - stonew[x]);
+            minvals.push_back(min);
+        }
+    }
+    auto& leastval= *min_element(minvals.begin(), minvals.end());
+    cout << leastval;
+
+
 }
---
+++ A2. Most Dangerous Gap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <random>
+using ll = long long;
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+}
