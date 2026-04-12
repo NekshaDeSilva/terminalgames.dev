ade01369beffa6686aa9284f9c58ce6371edbbea
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-12 17:03:49 +0530

    updated

---
+++ F_Movie_Festival.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <array>
+#include <cmath>
+#include <algorithm>
+#include <set>
+#include <utility>
+#include <random>
+#include <thread>
+#include <climits>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+using ll = long long;
+using iii = int;
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    //GO     
+
+
+    }
