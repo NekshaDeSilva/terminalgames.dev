29084d01cd7d10f1347c93feed5ea1b0c57e1899
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-12 17:59:01 +0530

    updated

--- F_Movie_Festival.cpp
+++ F_Movie_Festival.cpp
@@ -12,8 +12,6 @@
 #include <thread>
 #include <climits>
 #include <unordered_set>
-#include <unordered_map>
-#include <map>
 
 using namespace std;
 using ll = long long;
@@ -22,7 +20,33 @@ int main(){
 
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    //GO     
+    //GO        
+    ll numofc;
+    cin >> numofc;
+    vector<ll> listof0;
 
+    vector<pair<ll, ll>> times;
+    times.reserve(numofc);
+    ll temp1, temp2;
+    vector<ll> maxcelemtemp;
+    maxcelemtemp.reserve(1e9);
+    for(int i =0; i < numofc; i++){
+        cin >> temp1 >> temp2;
+        times.emplace_back(temp1 , temp2);
+        maxcelemtemp.push_back(temp2);
 
     }
+    auto maxofallsec = max_element(maxcelemtemp.begin(), maxcelemtemp.end());
+    listof0.reserve(*maxofallsec);
+    listof0.resize(*maxofallsec, 0);
+    sort(listof0.begin(), listof0.end());
+    for(int i =0 ; i < numofc; i ++){
+        for(int  x =(times[i].first-1);  x < (times[i].second); x++){
+            listof0[x] +=1;
+
+        }
+    }
+    
+    auto it = *max_element(listof0.begin(), listof0.end());
+    cout << it;
+}
