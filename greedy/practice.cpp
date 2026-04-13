09749c7ff76c69007576a890c3d248b5a4f7b30a
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-13 19:45:58 +0530

    updatex

--- greedy/practice.cpp
+++ greedy/practice.cpp
@@ -12,6 +12,9 @@
 #include <utility>
 #include <random>
 #include <numeric>
+/*the new libes that are needed*/
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 using ll = long long ;
@@ -20,6 +23,9 @@ using iii = int;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    map<string, int> details;
+    cout << "when you need to insetr valus into a map,  you have few options and the best option is to use the normal brackett method. in this waty, even if a elemnt does n not exist in thatr specific place, you will be able to insert that value as a news values and then do so. ";
+    details["something"] = 29049824;
+    cout << "\n" <<  details["something"]; //this prints oput like smooth butter.
     
-
 }
