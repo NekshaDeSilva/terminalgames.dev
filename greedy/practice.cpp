35f756eb0fbcda8105aca25b06cee45a6a8751ab
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-13 17:15:12 +0530

    updated

---
+++ greedy/practice.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <set>
+#include <unordered_set>/*this is forgetful*/
+#include <utility>
+#include <random>
+#include <numeric>
+
+using namespace std;
+using ll = long long ;
+using iii = int;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+
+}
