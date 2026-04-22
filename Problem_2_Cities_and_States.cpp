01fb85da67bf0e1ee64e8305e082cd5a38df667e
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-22 13:15:51 +0530

    updated

--- Problem_2_Cities_and_States.cpp
+++ Problem_2_Cities_and_States.cpp
@@ -19,8 +19,8 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    // freopen("citystate.in", "r" ,stdin);
-    // freopen("citystate.out", "w" , stdout);
+    freopen("citystate.in", "r" ,stdin);
+    freopen("citystate.out", "w" , stdout);
     ll numofcities  =0; 
     vector<string> maps;
     string tempcity;
@@ -30,11 +30,12 @@ int main(){
     for(ll i =0 ; i < numofcities; i ++){
         cin >> tempcity >> tempstate;
         cityneeded = tempcity.substr(0, 2);
-        if (cityneeded != tempstate) {
+        
             maps.push_back(cityneeded + tempstate);
-        }
+        
+
     }
-    // sort(maps.begin(),maps.end());
+    sort(maps.begin(),maps.end());
     ll counter = 0;
     for(auto i =maps.begin() ; i != maps.end() ; i++){
         for(auto x = next(i); x !=  maps.end(); x++){
@@ -48,5 +49,12 @@ int main(){
     }
 
     cout << counter;
-    
+    /*
+    4. How to spot this pattern in the future
+Next time you are in a contest, look for these clues to trigger this "encoding" method:
+
+Multiple properties belong to one entity: (e.g., A cow has a color and a weight, or a coordinate has an X and a Y).
+The properties have strict, predictable bounds: (e.g., Exactly 2 characters, or exactly 3 digits).
+You need to compare these entities quickly: Comparing one concatenated string ("MIFL" == "MIFL") is much faster and requires less code than comparing properties of structs (a.city == b.city && a.state == b.state).
+    */
  }  
