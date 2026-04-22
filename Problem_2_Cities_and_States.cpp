901e95b00d6a64e91a5c7545f2a7a62c68ec0ac8
Neksha DeSilva <rohandesilvahotmail@gmail.com>
2026-04-22 10:23:27 +0530

    updated

--- Problem_2_Cities_and_States.cpp
+++ Problem_2_Cities_and_States.cpp
@@ -19,27 +19,26 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    freopen("citystate.in", "r" ,stdin);
-    freopen("citystate.out", "w" , stdout);
+    // freopen("citystate.in", "r" ,stdin);
+    // freopen("citystate.out", "w" , stdout);
     ll numofcities  =0; 
-    map<string, ll> maps;
+    vector<string> maps;
     string tempcity;
     string tempstate;
     string cityneeded;
     cin>> numofcities;
     for(ll i =0 ; i < numofcities; i ++){
         cin >> tempcity >> tempstate;
-        cityneeded = tempcity[0] + tempcity[1];
-        maps.insert(cityneeded);
-        maps.insert(tempstate);
-
-    
+        cityneeded = tempcity.substr(0, 2);
+        if (cityneeded != tempstate) {
+            maps.push_back(cityneeded + tempstate);
+        }
     }
     // sort(maps.begin(),maps.end());
     ll counter = 0;
     for(auto i =maps.begin() ; i != maps.end() ; i++){
         for(auto x = next(i); x !=  maps.end(); x++){
-            if(*i == *x){
+            if((*i).substr(0,2) == (*x).substr(2,2) && (*i).substr(2,2) == (*x).substr(0,2)){
                 counter ++;
 
             }else{  
