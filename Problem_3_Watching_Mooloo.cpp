f301f8e55ab14cb2fbfa941a95feabd16279c925
NekshaDesilva <nekshavs@gmail.com>
2026-04-23 20:01:39 +0530

    updateds

--- Problem_3_Watching_Mooloo.cpp
+++ Problem_3_Watching_Mooloo.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <array>
 #include <vector>
+// #include <fstream>  a new lib just used for context
 using  ll =long long;
 using namespace std;
 int main(){
@@ -22,6 +23,44 @@ int main(){
     // freopen("breedflip.in", "r" ,stdin);
     // freopen("breedflip.out", "w" , stdout);
     ll n, k ;
+    cin >> n >> k;
+    vector<ll> days;
+    ll temp;
+    bool addedornot = false;
+
+    for(ll i=0;  i < n; i++){
+        cin >> temp;
+        days.push_back(temp);
+            
+    }
+    ll money = 0;
+    ll coverage =0;
+    ll others =0;
+    ll subc = k;
+    ll nomc = 1;
+    ll lastactive =0;
+    ll diff= 0;
+
+    for(ll i =0; i < n ;  i++){
+        if(i == 0){
+            money+= (subc + nomc);
+            lastactive = 0;
+
+            continue;
+        }
+        if((days[i] - days[lastactive]) <= subc ){
+            diff = days[i] - days[lastactive];
+            money+= (diff*1);
+lastactive = i;
+        }else if(days[i] - days[lastactive] > subc){
+            lastactive = i;
+            money += (subc + nomc);
+            // cout << "added: "<<  money << " --lastactiive: " << i << "--\n";
+
+        } 
     
+    }   
+
+    cout << money;
 
 }
