ef1056c0924916568364313ad3728c5cfa57b02e
NekshaDesilva <nekshavs@gmail.com>
2026-04-23 17:47:42 +0530

    up[dated

--- Problem_2_Mad_Scientist.cpp
+++ Problem_2_Mad_Scientist.cpp
@@ -19,7 +19,55 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL);
-    // freopen("citystate.in", "r" ,stdin);
-    // freopen("citystate.out", "w" , stdout);
+    freopen("breedflip.in", "r" ,stdin);
+    freopen("breedflip.out", "w" , stdout);
+    ll numofelms = 0;
+    cin >> numofelms;
+    vector<char> target;
+    vector<char> whatwehave;
+    whatwehave.reserve(numofelms);
+    target.reserve(numofelms);
+    char temp ;
+
+    for(ll i =0; i < numofelms; i++){
+        cin >> temp;
+        target.push_back(temp);
+        
+    }
+    for(ll i =0;  i < numofelms ; i++){
+        cin >> temp;
+        whatwehave.push_back(temp);
+
+    }
+    vector<ll> strtaken;
+    ll counterx  =0;
+
+    for(ll i =0 ; i < numofelms ; i ++){
+        
+        if(target[i] == whatwehave[i] ){
+            
+            if(strtaken.size() != 0){
+                  counterx++;
+strtaken.clear();
+                for(ll x = 0;  x < strtaken.size(); x++){
+                    if(whatwehave[x] == 'H'){
+                        whatwehave[x] = 'G';
+                        
+                    }else{
+                        whatwehave[x] = 'H';
+
+                    }
+                }
+            }
+            continue;
+        }else{
+            strtaken.push_back(i);
+            
+            
+          
+        }
+    }
+    cout << counterx;
+
 
 }
