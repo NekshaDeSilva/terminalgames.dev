be442d44e5bdb1a526e2966b581d09fea8fc9dbb
NekshaDesilva <nekshavs@gmail.com>
2026-04-24 06:11:42 +0530

    updated

--- Problem_2_Even_More_Odd_Photos.cpp
+++ Problem_2_Even_More_Odd_Photos.cpp
@@ -22,4 +22,38 @@ int main(){
     cin.tie(NULL);
     // freopen("breedflip.in", "r" ,stdin);
     // freopen("breedflip.out", "w" , stdout);
+    ll numofcows;
+    cin >> numofcows;
+
+    int evens = 0;
+    int odds = 0;
+    int temp;
+
+    for(int i =0; i < numofcows ;  i ++){
+        cin>> temp;
+        if(temp % 2 == 0) {
+            evens++;
+        } else {
+            odds++;
+        }
+    }
+    
+    // We want to alternate: Even, Odd, Even, Odd...
+    // If we have too many odds, we can combine 2 odds together to make 1 even!
+    while (odds > evens) {
+        odds -= 2;
+        evens++;
+    }
+    
+    int counter = 0;
+    // If we have perfectly alternating piles
+    if (evens == odds) {
+        counter = evens + odds;
+    } 
+    // If we have more evens than odds, the leftovers just get dumped into the first Even group
+    else if (evens > odds) {
+        counter = (odds * 2) + 1;
+    }
+    cout << counter;
+
 }
