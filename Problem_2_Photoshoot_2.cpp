ded846a209f55e2bb957e66e69eb1a6cb7f9a391
NekshaDesilva <nekshavs@gmail.com>
2026-04-24 13:27:40 +0530

    updated

--- Problem_2_Photoshoot_2.cpp
+++ Problem_2_Photoshoot_2.cpp
@@ -22,5 +22,47 @@ int main(){
     cin.tie(NULL);
     // freopen("breedflip.in", "r" ,stdin);
     // freopen("breedflip.out", "w" , stdout);
+    ll numofcows;
+    cin>> numofcows;
+    vector<ll > cows;
+    vector<ll> need;
+    ll temp;
+
+    for(ll i =0;  i < numofcows*2; i++){
+         if(i >= numofcows){
+            int newi  = (i-numofcows);
+            cin >> temp;
+            need.push_back(temp);
+            continue;
+        }
+        cin >> temp;
+        cows.push_back(temp);
+       
+    }
+    ll counter = 0;
+
+    for(ll i= 0; i < numofcows ; i++){
+        if(cows[i] == need[i] ){
+            
+            continue;
+
+        }
+    else if(cows[i] != need[i]){
+        counter++;
+        
+
+        auto loc1 = find(cows.begin(), cows.end(), need[i]);
+        ll loc2= (loc1 - cows.begin());
+        ll temp2 = cows[loc2] ;
+        
+        for(ll o = loc2; o > i; o--){
+            cows[o] = cows[o-1];
+        }
+        cows[i] = temp2;
+
+    }
+
+    }
+    cout << counter;
     
 }
