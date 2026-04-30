e8fc5e4d3cb7869e12fba17e7ad3e50e2c7dd04f
NekshaDesilva <nekshavs@gmail.com>
2026-04-30 15:36:26 +0530

    updated

--- T_Room_Allocation.cpp
+++ T_Room_Allocation.cpp
@@ -1,10 +1,22 @@
-/*Nekshavs*/
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*nekshavs*/
 #include <queue>
-
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <random>
+#include <climits>
+#include <cmath>
+#include <cstdio>
 using namespace std;
+using ll = long long;
+#define pb push_back(x)
+#define eb emplace_back(x)
 
 struct Customer {
     int start;
@@ -18,41 +30,45 @@ bool compareCustomers(const Customer& a, const Customer& b) {
     return a.start < b.start;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
+int main(){
+    ios_base::sync_with_stdio (false);
     cin.tie(NULL);
-
+    
     int n;
     if (!(cin >> n)) return 0;
 
-    vector<Customer> customers(n);
+    vector<Customer> people(n);
     for (int i = 0; i < n; ++i) {
-        cin >> customers[i].start >> customers[i].end;
-        customers[i].index = i;
+        cin >> people[i].start >> people[i].end;
+        people[i].index = i;
     }
 
-    sort(customers.begin(), customers.end(), compareCustomers);
+    sort(people.begin(), people.end(), compareCustomers);
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    vector<int> room_assigned(n);
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+    
+    greater<pair<int, int>>> pq;
+    vector<int> roomse(n);
 
     int rooms = 0;
     for (int i = 0; i < n; ++i) {
-        if (pq.empty() || pq.top().first >= customers[i].start) {
+        if (pq.empty() || pq.top().first >= people[i].start) {
             rooms++;
-            pq.push({customers[i].end, rooms});
-            room_assigned[customers[i].index] = rooms;
+            pq.push({people[i].end, rooms});
+            roomse[people[i].index] = rooms;
         } else {
+         
             int room = pq.top().second;
             pq.pop();
-            pq.push({customers[i].end, room});
-            room_assigned[customers[i].index] = room;
+            pq.push({people[i].end, room});
+            roomse[people[i].index] = room;
         }
     }
 
-    cout << rooms << "\n";
+    cout << rooms <<    "\n";
     for (int i = 0; i < n; ++i) {
-        cout << room_assigned[i] << (i == n - 1 ? "" : " ");
+        if (i) cout << ' ';
+        cout << roomse[i];
     }
     cout << "\n";
 
