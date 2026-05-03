17040ae2d5882f16354ee33daed9572f1495b960
NekshaDesilva <nekshavs@gmail.com>
2026-05-03 13:43:11 +0530

    updatex

--- J_Missing_Coin_Sum.cpp
+++ J_Missing_Coin_Sum.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-#include <iomanip>
+#include  <iomanip>
 #include <string>
+#include <vector>
+#include <unordered_map>
 #include <map>
 #include <set>
 #include <unordered_set>
-#include <unordered_map>
-#include <vector>
-#include <fstream>
-#include <climits>
-#include <cmath>
-#include <algorithm>
 #include <numeric>
-#include <cstdio>
-#include <random>
+#include <algorithm>
+#include <cmath>
+#include <climits>
 #include <utility>
-
+#include <random>
+#include <cstidio>
 using namespace std;
 using ll = long long;
-using  ss = string;
-using dbl = double;
-
+using iii = int;
+using ss  = string;
+using dd  = double;
 #define pb(x) push_back(x)
 #define eb(x) emplace_back(x)
 
 int main(){
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-
+    //go
+    
 }
