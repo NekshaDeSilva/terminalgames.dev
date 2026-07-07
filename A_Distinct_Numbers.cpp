32c61501bd951f5d274ab4981e369fcb677c2c25
Neksha DeSilva <nekshavs@gmail.com>
2026-07-07 20:59:23 +0530

    updated

--- A_Distinct_Numbers.cpp
+++ A_Distinct_Numbers.cpp
@@ -1,31 +1,48 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <algorithm>
 #include <cmath>
-#include <vector>
-#include <array>
+#include <ctime>
+#include <random>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <algorithm>
+#include <random>
+#include <>
 
-using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-//go
-    long long int numofvals;
-    cin >> numofvals;
-    long long tempval;
-    vector<long long> list;
-    list.reserve(numofvals);
-    for(long long i =0; i < numofvals;  i++){
-        cin >> tempval;
-        list.push_back(tempval);
+
+// #include <iostream>
+// #include <iomanip>
+// #include <string>
+// #include <algorithm>
+// #include <cmath>
+// #include <vector>
+// #include <array>
+
+// using namespace std;
+
+// int main(){
+//     ios_base::sync_with_stdio(0);
+//     cin.tie(NULL);
+// //go
+//     long long int numofvals;
+//     cin >> numofvals;
+//     long long tempval;
+//     vector<long long> list;
+//     list.reserve(numofvals);
+//     for(long long i =0; i < numofvals;  i++){
+//         cin >> tempval;
+//         list.push_back(tempval);
 
         
-    }
-    sort(list.begin(), list.end());
-    int count=0;
-    auto distinct = unique(list.begin(), list.end());
-    cout << distance(list. begin(), distinct);
+//     }
+//     sort(list.begin(), list.end());
+//     int count=0;
+//     auto distinct = unique(list.begin(), list.end());
+//     cout << distance(list. begin(), distinct);
 
-}
+// }
